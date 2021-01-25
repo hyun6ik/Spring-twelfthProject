635940468d6c4bd66fabe986ba95112f3c885ace
hyun6ik <fkanwm@naver.com>
2021-01-25 23:36:27 +0900

    프로젝트 생성

---
+++ spring_data_jpa_repeat/src/main/java/study/spring_data_jpa_repeat/SpringDataJpaRepeatApplication.java
@@ -0,0 +1,13 @@
+package study.spring_data_jpa_repeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaRepeatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaRepeatApplication.class, args);
+	}
+
+}
---
+++ spring_data_jpa_repeat/src/test/java/study/spring_data_jpa_repeat/SpringDataJpaRepeatApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_data_jpa_repeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaRepeatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
