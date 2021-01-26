281bec60f8fcd6871a639110196c39c51301d6b4
hyun6ik <fkanwm@naver.com>
2021-01-26 09:02:46 +0900

    예제 도메인 모델과 동작확인

--- spring_data_jpa_repeat/src/main/java/study/spring_data_jpa_repeat/entitiy/Member.java
+++ spring_data_jpa_repeat/src/main/java/study/spring_data_jpa_repeat/entitiy/Member.java
@@ -1,24 +1,42 @@
 package study.spring_data_jpa_repeat.entitiy;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
 public class Member {
 
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null){
+            changeTeam(team);
+        }
+    }
+
+    //연관관계메서드
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
