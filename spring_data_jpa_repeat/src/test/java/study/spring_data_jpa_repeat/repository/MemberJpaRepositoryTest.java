ce1adc62b371a4ae7bfba79ddab7fa5c9df03013
hyun6ik <fkanwm@naver.com>
2021-01-28 05:48:14 +0900

    순수 JPA 기반 리포지토리 만들기

--- spring_data_jpa_repeat/src/test/java/study/spring_data_jpa_repeat/repository/MemberJpaRepositoryTest.java
+++ spring_data_jpa_repeat/src/test/java/study/spring_data_jpa_repeat/repository/MemberJpaRepositoryTest.java
@@ -8,6 +8,9 @@ import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.spring_data_jpa_repeat.entitiy.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,5 +33,41 @@ class MemberJpaRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(savedMember.getId());
 
     }
+
+    @Test
+    public void basicCRUD() throws Exception {
+        //given
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+        //when
+        // 단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).orElse(null);
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).orElse(null);
+
+        // 리스트 조회 검증
+        List<Member> result = memberJpaRepository.findAll();
+
+        // 카운트 검증
+        long count = memberJpaRepository.count();
+
+        // 삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+        long deletedCount = memberJpaRepository.count();
+        //then
+        assertThat(member1.getId()).isEqualTo(findMember1.getId());
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        assertThat(result.size()).isEqualTo(2);
+
+        assertThat(count).isEqualTo(2);
+        assertThat(deletedCount).isEqualTo(0);
+
+
+    }
+
 
 }
